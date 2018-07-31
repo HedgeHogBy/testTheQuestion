e5320ad50f6eef084fb66ea9966ca7e2844f74e0
Eugene Stankevich <stankevich.e@gmail.com>
2018-07-31 11:32:44 +0300

    add search page structure and routing

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SearchPageComponent } from './search-page/search-page.component';
+import { SearchInputComponent } from './search-input/search-input.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { ResultCardComponent } from './result-card/result-card.component';
+
+import { SearchRoutingModule } from './search-routing.module';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SearchRoutingModule
   ],
-  declarations: []
+  declarations: [SearchPageComponent, SearchInputComponent, SearchResultsComponent, ResultCardComponent],
+  exports: [SearchPageComponent]
 })
 export class SearchModule { }
